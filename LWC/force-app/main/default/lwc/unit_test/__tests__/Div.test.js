786078918235ba62c223fa6930d6bed7520f3917
saiveer757 <60533901+saiveer757@users.noreply.github.com>
2021-07-01 10:57:52 +0530

    Addition, Subtraction

---
+++ LWC/force-app/main/default/lwc/unit_test/__tests__/Div.test.js
@@ -0,0 +1,8 @@
+import { mul } from '../mul'
+
+describe('div()', () => {
+    it('should div 4 and 2 returning 2', () => {
+        expect(div(1, 2)).toBe(2);
+      });
+
+})
--- LWC/force-app/main/default/lwc/unit_test/__tests__/mul.test.js
+++ LWC/force-app/main/default/lwc/unit_test/__tests__/mul.test.js
@@ -1,8 +1,8 @@
 import { mul } from '../mul'
 
 describe('mul()', () => {
-    it('should mul 1 and 2 returning 2', () => {
-        expect(mul(1, 2)).toBe(2);
+    it('should mul 6 and 2 returning 3', () => {
+        expect(mul(6, 2)).toBe(3);
       });
 
 })
